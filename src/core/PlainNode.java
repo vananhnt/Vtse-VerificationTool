26e6021dcbcd54c44cce2f96723d3750b6afe5f9
nguyenthithuy <ntthuyyl@gmail.com>
2017-07-06 15:24:43 +0700

    third

--- src/core/PlainNode.java
+++ src/core/PlainNode.java
@@ -2,8 +2,9 @@ package core;
 // node is 1 incommingNode and 1 outGoingNode;
 
 import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTExpressionStatement;
 
 public class PlainNode {
-	private IASTNode data;
+	private CPPASTExpressionStatement expression;
 	
 }
