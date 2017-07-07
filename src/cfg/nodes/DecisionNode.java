3151f3254f9029c4df1c56fc8c5a4d2d6fb55777
nguyenthithuy <ntthuyyl@gmail.com>
2017-07-07 11:07:58 +0700

    add::  get with Input is IASTstatement

--- src/cfg/nodes/DecisionNode.java
+++ src/cfg/nodes/DecisionNode.java
@@ -1,10 +1,12 @@
 package cfg.nodes;
 
+import org.eclipse.cdt.core.dom.ast.IASTExpression;
+import org.eclipse.cdt.core.dom.ast.IASTStatement;
 import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTExpressionStatement;
 import org.eclipse.cdt.internal.core.util.ToStringSorter;
 
 public class DecisionNode extends CFGNode{
-	private CPPASTExpressionStatement condition;
+	private IASTExpression condition;
 	private CFGNode thenNode;
 	private EndConditionNode endNode;
 	// elseNode is next
@@ -23,18 +25,38 @@ public class DecisionNode extends CFGNode{
 	
 	
 	
-	public CPPASTExpressionStatement getCondition() {
+	public IASTExpression getCondition() {
 		return condition;
 	}
-	public void setCondition(CPPASTExpressionStatement condition) {
-		this.condition = condition;
+	public void setCondition(IASTExpression iastExpression) {
+		this.condition = iastExpression;
 	}
+	
+//	set THEN NODE with Input is CFGNode  or  IASTStatement		
 	public CFGNode getThenNode() {
 		return thenNode;
-	}
+	}	
 	public void setThenNode(CFGNode thenNode) {
 		this.thenNode = thenNode;
 	}
+	public void setThenNode( IASTStatement statement){
+		this.thenNode.setData( statement);
+	}
+	
+// set ELSE NODE with Input is CFGNode  or  IASTStatement	
+	public void setElseNode( CFGNode elseNode){
+		this.setNext(elseNode);
+	}
+	public void setElseNode( IASTStatement statement){
+		CFGNode node = this.getNext();
+		node.setData(statement);
+		this.setNext(node);
+	}
+	public CFGNode getElseNode(){
+		return this.getNext();
+	}
+	
+	
 	public EndConditionNode getEndNode() {
 		return endNode;
 	}
