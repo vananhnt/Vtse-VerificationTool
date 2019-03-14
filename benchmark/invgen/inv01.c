04a14004646707a81dcec471fedb95eb18873793
vananhnt97 <vananhnt97@gmail.com>
2019-03-14 23:36:51 +0700

    resolve confilcs

--- benchmark/invgen/inv01.c
+++ benchmark/invgen/inv01.c
@@ -1,13 +1,12 @@
-int inv01() {
+void inv01()
+{
  int x=1; int y=1;
- while(1 > 0) {
-   invariant: y >= 1 ;
+ while(__BLAST_NONDET) {
+   invariant: x = y ;
    int t1 = x;
    int t2 = y;
    x = t1 + t2;
    y = t1 + t2;
  }
-   y = -y;
-   return y;
-   //post: y >= 1;
+   post: y >= 1;
 }
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,40 +1,34 @@
 package test;
 
-import app.verification.ExportExcel;
-import app.verification.FileVerification;
-import app.verification.report.VerificationReport;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.List;
+import java.io.*;
 import java.util.Properties;
+
+import cfg.build.ASTFactory;
+import cfg.build.VtseCFG;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPNodeFactory;
 
 /**
  * @author va
+ *
  */
 public class Test {
-    private static String getBenchmark(String props) throws IOException {
-        String path = System.getProperty("user.dir") + "/src/test/benchmark.properties";
-        FileInputStream is = new FileInputStream(new File(path));
-        Properties benchmarkProps = new Properties();
-        benchmarkProps.load(is);
-        return benchmarkProps.getProperty("invgen");
-    }
+	private static String getBenchmark(String props) throws IOException {
+		String path = System.getProperty("user.dir") + "/src/test/benchmark.properties";
+		FileInputStream is = new FileInputStream(new File(path));
+		Properties benchmarkProps = new Properties();
+		benchmarkProps.load(is);
+		return benchmarkProps.getProperty("invgen");
+	}
 
-    public static void main(String[] args) throws Exception {
-        String benchmark = getBenchmark("invgen") + "inv01.c";
-        //ASTFactory ast = new ASTFactory(benchmark);
-        //VtseCFG cfg = new VtseCFG(ast.getFunction(0), ast);
-        //cfg.invariant();
-        //cfg.printGraph();
-        //cfg.printMeta();
+	public static void  main(String[] args) throws FileNotFoundException, IOException {
+		String benchmark = getBenchmark("invgen") + "inv01.c";
+		ASTFactory ast = new ASTFactory(benchmark);
+		VtseCFG cfg = new VtseCFG(ast.getFunction(0), ast);
+//		cfg.unfold();
+//		cfg.index();
+		cfg.invariant();
+		//ast.print();
+		cfg.printGraph();
 
-        ExportExcel exportExcel = new ExportExcel();
-        File file = new File(benchmark);
-        FileVerification fv = new FileVerification();
-        List<VerificationReport> reportList = fv.verifyDirectory(file);
-        exportExcel.writeExcel(reportList);
-
-    }
+	}
 }
