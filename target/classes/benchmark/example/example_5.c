d018cbee184834d28256bc43bdba0964319a8aaa
vananhnt97 <vananhnt97@gmail.com>
2020-07-28 22:18:43 +0900

    remove redundances

--- target/classes/benchmark/example/example_5.c
+++
@@ -1,9 +0,0 @@
-float foo (float a, float b) {
-	float result = 0;
-	if (a > b) {
-		result = a / (b - 2);	
-	} else {
-		result = a + b;
-	}	
-	return result;
-}
--- target/classes/benchmark/float-cdfpl-func/sine_4_true_unreach_call.c
+++
@@ -1,6 +0,0 @@
-double sine_4_true_unreach_call(float IN)
-{
-  float x = IN;
-  float result = x - (x*x*x)/6.0 + (x*x*x*x*x)/120.0 + (x*x*x*x*x*x*x)/5040.0;
-  return result;
-}
